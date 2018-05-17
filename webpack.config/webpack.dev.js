282513875ba79adea8590b4b582ea1049973ce12
Alan Chen <739709491@qq.com>
2018-05-17 19:38:19 +0800

    Add webpack.dev config

--- webpack.config/webpack.dev.js
+++ webpack.config/webpack.dev.js
@@ -1,15 +1,20 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.base.js')
+const host = require('./config').host
 const port = require('./config').port
+const autoOpen = require('./config').autoOpen
 
 module.exports = merge(common, {
 	devServer: {
 		contentBase: process.cwd(),//当前是以项目根目录作为本地服务器根目录
-		port: port,
+		host,
+		port,
 		hot: true,
 		inline: true,
+		open: autoOpen,
 		openPage: 'dist/index.html',
 		historyApiFallback: true,
+		quiet: true, //ErrorFriendly插件要求，开发环境出错不会出现打包信息
 		noInfo: true,//不显示打包的信息
 		overlay: {//全屏显示错误和警告
 			warnings: true,
