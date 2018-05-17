13d09d68fbbeaeaa37e3e9de95a15c8d87be604f
Alan Chen <739709491@qq.com>
2018-05-17 19:39:42 +0800

    Add configs

--- webpack.config/config.js
+++ webpack.config/config.js
@@ -1,6 +1,8 @@
-//此处设置devServer的端口和需要单独打包的第三方库,注意：如果想添加库，必须先install！否则会报错
+//此处设置devServer的ip地址、端口和需要单独打包的第三方库,注意：如果想添加库，必须先install！否则会报错
 module.exports = {
+    host: 'localhost',
     port:4000,
+    autoOpen: false, //是否自动打开页面
     vendors:[
         'react',
         'react-dom',
